efaf37217967464728c8a39ac7ae620d3b7a3d31
LAV45 <LAV451@gmail.com>
2015-01-21 21:42:59 +0300

    UPGRADE to 1.1.0

--- AceEditorWidget.php
+++ AceEditorWidget.php
@@ -2,19 +2,17 @@
 /**
  * @copyright Copyright &copy; Aleksey Loban, lav451@gmail.com, 2014
  * @package lav45/yii2-aceeditor
- * @version 1.0.0
- *
- * echo $form->field($model, 'text')->widget(AceEditorWidget::className());
+ * @version 1.1.0
  *
  * echo $form->field($model, 'content')->widget(AceEditorWidget::className(), [
- *     'containerOptions' => [
- *         'id' => 'editor',
- *         'style' => 'width: 100%; min-height: 400px;',
- *     ],
- *     'editorSettings' => "
- *         editor.setTheme('ace/theme/idle_fingers');
- *         editor.getSession().setMode('ace/mode/html');
- *     ",
+ *     'theme' => 'xcode',
+ *     'mode' => 'html',
+ *     'showPrintMargin' => false,
+ *     'fontSize' => 14,
+ *     'height' => 400,
+ *     'options' => [
+ *         'style' => 'border: 1px solid #ccc; border-radius: 4px;'
+ *     ]
  * ]);
  *
  * OR
@@ -22,48 +20,445 @@
  * // Ace editor without model
  * echo AceEditorWidget::widget([
  *     'name' => 'description',
- *     'value' => 'same text',
+ *     'value' => 'text value',
+ *     'theme' => 'idle_fingers',
+ *     'mode' => 'html',
  * ]);
  */
 
 namespace lav45\aceEditor;
 
 use yii\helpers\Html;
-use yii\helpers\Inflector;
 use yii\widgets\InputWidget;
 
 class AceEditorWidget extends InputWidget
 {
     /**
-     * This configuration of the container in which you will install the editor
-     * @var array Div options
-     */
-    public $containerOptions = [
-        'style' => 'min-height: 400px;'
-    ];
-
-    /**
      * @var string JS Settings for AceEditor
-     * @link http://ace.c9.io/#nav=howto
      */
-    public $editorSettings = "
-        editor.setTheme('ace/theme/idle_fingers');
-        editor.getSession().setMode('ace/mode/html');
-    ";
+    private $_editorSettings;
 
     /**
-     * @inheritdoc
+     * The height of the editor
+     * @param integer
      */
-    public function init()
+    public $height = 300;
+
+    /**
+     * @var boolean
+     */
+    private $_read_only = false;
+
+    /**
+     * Using this method, you can add arbitrary javascript code
+     * @param $str string
+     */
+    public function setEditorSettings($str)
     {
-        parent::init();
+        $this->addSettings($str);
+    }
 
-        if (!isset($this->containerOptions['id'])) {
-            $this->containerOptions['id'] = $this->options['id'] . '-editor';
-        }
+    /**
+     * Themes are loaded on demand; all you have to do is pass the string name:
+     *
+     * @param $value string
+     * - ambiance
+     * - chaos
+     * - chrome
+     * - clouds
+     * - clouds_midnight
+     * - cobalt
+     * - crimson_editor
+     * - dawn
+     * - dreamweaver
+     * - eclipse
+     * - github
+     * - idle_fingers
+     * - katzenmilch
+     * - kr_theme
+     * - kuroir
+     * - merbivore
+     * - merbivore_soft
+     * - mono_industrial
+     * - monokai
+     * - pastel_on_dark
+     * - solarized_dark
+     * - solarized_light
+     * - terminal
+     * - textmate
+     * - tomorrow
+     * - tomorrow_night_blue
+     * - tomorrow_night_bright
+     * - tomorrow_night_eighties
+     * - tomorrow_night
+     * - twilight
+     * - vibrant_ink
+     * - xcode
+     */
+    public function setTheme($value)
+    {
+        $this->addSettings("editor.setTheme('ace/theme/$value');");
+    }
 
-        $this->registerAssets();
-        $this->registerJS();
+    /**
+     * By default, the editor supports plain text mode. All other language modes are available
+     * as separate modules, loaded on demand like this:
+     *
+     * @param $value string
+     * - abap
+     * - actionscript
+     * - ada
+     * - apache_conf
+     * - applescript
+     * - asciidoc
+     * - assembly_x86
+     * - autohotkey
+     * - batchfile
+     * - c9search
+     * - c_cpp
+     * - cirru
+     * - clojure
+     * - cobol
+     * - coffee
+     * - coldfusion
+     * - csharp
+     * - css
+     * - curly
+     * - dart
+     * - diff
+     * - django
+     * - d
+     * - dockerfile
+     * - dot
+     * - eiffel
+     * - ejs
+     * - erlang
+     * - forth
+     * - ftl
+     * - gcode
+     * - gherkin
+     * - gitignore
+     * - glsl
+     * - golang
+     * - groovy
+     * - haml
+     * - handlebars
+     * - haskell
+     * - haxe
+     * - html
+     * - html_ruby
+     * - ini
+     * - io
+     * - jack
+     * - jade
+     * - java
+     * - javascript
+     * - jsoniq
+     * - json
+     * - jsp
+     * - jsx
+     * - julia
+     * - latex
+     * - less
+     * - liquid
+     * - lisp
+     * - livescript
+     * - logiql
+     * - lsl
+     * - lua
+     * - luapage
+     * - lucene
+     * - makefile
+     * - markdown
+     * - matlab
+     * - mel
+     * - mushcode
+     * - mysql
+     * - nix
+     * - objectivec
+     * - ocaml
+     * - pascal
+     * - perl
+     * - pgsql
+     * - php
+     * - plain_text
+     * - powershell
+     * - praat
+     * - prolog
+     * - properties
+     * - protobuf
+     * - python
+     * - rdoc
+     * - rhtml
+     * - r
+     * - ruby
+     * - rust
+     * - sass
+     * - scad
+     * - scala
+     * - scheme
+     * - scss
+     * - sh
+     * - sjs
+     * - smarty
+     * - snippets
+     * - soy_template
+     * - space
+     * - sql
+     * - stylus
+     * - svg
+     * - tcl
+     * - tex
+     * - textile
+     * - text
+     * - toml
+     * - twig
+     * - typescript
+     * - vala
+     * - vbscript
+     * - velocity
+     * - verilog
+     * - vhdl
+     * - xml
+     * - xquery
+     * - yaml
+     */
+    public function setMode($value)
+    {
+        $this->addSettings("editor.session.setMode('ace/mode/$value');");
+    }
+
+    /**
+     * Font size text in editor
+     * @param $value integer
+     */
+    public function setFontSize($value)
+    {
+        $this->addSettings("editor.setFontSize($value);");
+    }
+
+    /**
+     * Code Folding
+     *
+     * @param $value string
+     *  - manual
+     *  - markbegin
+     *  - markbeginend
+     */
+    public function setFoldStyle($value)
+    {
+        $this->addSettings("
+            editor.session.setFoldStyle('$value');
+            editor.setShowFoldWidgets($value !== 'manual');
+        ");
+    }
+
+    /**
+     * Key Binding
+     *
+     * @param $value string
+     * - ace
+     * - vim
+     * - emacs
+     * - custom
+     */
+    public function setKeyBinding($value)
+    {
+        $this->addSettings("editor.setKeyboardHandler('$value');");
+    }
+
+    /**
+     * Soft Wrap
+     *
+     * @param $value integer|string
+     * - off
+     * - 40
+     * - 80
+     * - free
+     */
+    public function setSoftWrap($value)
+    {
+        $this->addSettings("editor.setOption('wrap', '$value');");
+    }
+
+    /**
+     * Full Line Selection
+     * @param $value boolean
+     */
+    public function setSelectionStyle($value)
+    {
+        $value = $value ? 'line' : 'text';
+        $this->addSettings("editor.setOption('selectionStyle', $value);");
+    }
+
+    /**
+     * Highlight Active Line
+     * @param $value boolean
+     */
+    public function setHighlightActiveLine($value)
+    {
+        $value = var_export($value, true);
+        $this->addSettings("editor.setHighlightActiveLine($value);");
+    }
+
+    /**
+     * Show Invisibles
+     * @param $value boolean
+     */
+    public function setShowInvisibles($value)
+    {
+        $value = var_export($value, true);
+        $this->addSettings("editor.setShowInvisibles($value);");
+    }
+
+    /**
+     * Show Indent Guides
+     * @param $value boolean
+     */
+    public function setDisplayIndentGuides($value)
+    {
+        $value = var_export($value, true);
+        $this->addSettings("editor.setDisplayIndentGuides($value);");
+    }
+
+    /**
+     * Show height scroll bar always visible
+     * @param $value boolean
+     */
+    public function setHScrollBar($value)
+    {
+        $value = var_export($value, true);
+        $this->addSettings("editor.setOption('hScrollBarAlwaysVisible', $value);");
+    }
+
+    /**
+     * Show vertical scroll bar always visible
+     * @param $value boolean
+     */
+    public function setVScrollBar($value)
+    {
+        $value = var_export($value, true);
+        $this->addSettings("editor.setOption('vScrollBarAlwaysVisible', $value);");
+    }
+
+    /**
+     * Show Animate scrolling
+     * @param $value boolean
+     */
+    public function setAnimatedScroll($value)
+    {
+        $value = var_export($value, true);
+        $this->addSettings("editor.setAnimatedScroll($value);");
+    }
+
+    /**
+     * Show Gutter
+     * @param $value boolean
+     */
+    public function setShowGutter($value)
+    {
+        $value = var_export($value, true);
+        $this->addSettings("editor.renderer.setShowGutter($value);");
+    }
+
+    /**
+     * Show Print Margin
+     * @param $value boolean
+     */
+    public function setShowPrintMargin($value)
+    {
+        $value = var_export($value, true);
+        $this->addSettings("editor.renderer.setShowPrintMargin($value);");
+    }
+
+    /**
+     * Use Soft Tab
+     * @param $value boolean
+     */
+    public function setUseSoftTabs($value)
+    {
+        $value = var_export($value, true);
+        $this->addSettings("editor.session.setUseSoftTabs($value);");
+    }
+
+    /**
+     * Highlight selected word
+     * @param $value boolean
+     */
+    public function setHighlightSelectedWord($value)
+    {
+        $value = var_export($value, true);
+        $this->addSettings("editor.setHighlightSelectedWord($value);");
+    }
+
+    /**
+     * Enable Behaviours
+     * @param $value boolean
+     */
+    public function setBehavioursEnabled($value)
+    {
+        $value = var_export($value, true);
+        $this->addSettings("editor.setBehavioursEnabled($value);");
+    }
+
+    /**
+     * Fade Fold Widgets
+     * @param $value boolean
+     */
+    public function setFadeFoldWidgets($value)
+    {
+        $value = var_export($value, true);
+        $this->addSettings("editor.setFadeFoldWidgets($value);");
+    }
+
+    /**
+     * Enable Elastic Tabstops
+     * @param $value boolean
+     */
+    public function setUseElasticTabstops($value)
+    {
+        $value = var_export($value, true);
+        $this->addSettings("editor.setOption('useElasticTabstops', $value);");
+    }
+
+    /**
+     * Incremental Search
+     * @param $value boolean
+     */
+    public function setUseIncrementalSearch($value)
+    {
+        $value = var_export($value, true);
+        $this->addSettings("editor.setOption('useIncrementalSearch', $value);");
+    }
+
+    /**
+     * Read-only
+     * @param $value boolean
+     */
+    public function setReadOnly($value)
+    {
+        $this->_read_only = $value;
+        $value = var_export($value, true);
+        $this->addSettings("editor.setReadOnly($value);");
+    }
+
+    /**
+     * Scroll Past End
+     * @param $value boolean
+     */
+    public function setScrollPastEnd($value)
+    {
+        $value = var_export($value, true);
+        $this->addSettings("editor.setOption('scrollPastEnd', $value);");
+    }
+
+    /**
+     * Append to general settings
+     * @param $str string
+     */
+    protected function addSettings($str)
+    {
+        $this->_editorSettings .= "$str\n";
     }
 
     /**
@@ -71,19 +466,33 @@ class AceEditorWidget extends InputWidget
      */
     public function run()
     {
-        $div = Html::tag('div', '', $this->containerOptions);
+        $this->registerAssets();
+        $this->registerJS();
 
-        Html::addCssStyle($this->options, 'display: none;');
-
-        if ($this->hasModel()) {
-            return $div . Html::activeTextarea($this->model, $this->attribute, $this->options);
-        } else {
-            return $div . Html::textarea($this->name, $this->value, $this->options);
+        if ($this->height !== null) {
+            Html::addCssStyle($this->options, "height: {$this->height}px;");
         }
+
+        $content = Html::tag('div', '', $this->options);
+
+        $containerOptions = [
+            'style' => 'display: none;',
+            'id' => $this->options['id'] . '-container'
+        ];
+
+        if ($this->_read_only !== false) {
+            $containerOptions['readonly'] = '';
+        }
+
+        $content .= $this->hasModel() ?
+            Html::activeTextarea($this->model, $this->attribute, $containerOptions) :
+            Html::textarea($this->name, $this->value, $containerOptions);
+
+        return $content;
     }
 
     /**
-     * Registers the needed assets
+     * Registers assets bundle
      */
     public function registerAssets()
     {
@@ -95,21 +504,18 @@ class AceEditorWidget extends InputWidget
      */
     public function registerJS()
     {
-        $text = Inflector::slug($this->options['id'], '_');
-        $editor = Inflector::slug($this->containerOptions['id'], '_');
-
         $this->getView()->registerJs("
-            var $text = $('#{$this->options['id']}');
-            var $editor = ace.edit('{$this->containerOptions['id']}');
+            (function($){
+                var text = $('#{$this->options['id']}-container');
+                var editor = ace.edit('{$this->options['id']}');
 
-            $editor.getSession().setValue($text.val());
-            $editor.getSession().on('change', function(){
-                $text.val($editor.getSession().getValue());
-            });
+                editor.getSession().setValue(text.val());
+                editor.getSession().on('change', function(){
+                    text.val(editor.getSession().getValue());
+                });
 
-            (function(editor){
-                {$this->editorSettings}
-            }($editor));
+                {$this->_editorSettings}
+            }(jQuery));
         ");
     }
 }
--- README.md
+++ README.md
@@ -9,45 +9,34 @@ This extension allows you to install and use the code editor [Ace](http://ace.c9
 
 The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
 
-> Note: You must set the `minimum-stability` to `dev` in the **composer.json** file in your application root folder before installation of this extension.
+Either run
 
-Either of your `composer.json` file
+```
+php composer.phar require --prefer-dist lav45/yii2-aceeditor "*"
+```
+
+or add
 
 ```json
-    "minimum-stability": "dev",
-    "require": {
-        "lav45/yii2-aceeditor": "dev-master"
-    }
+	"lav45/yii2-aceeditor": "*"
 ```
 
 ## Usage
 
 ### How to call?
 ```php
-	// add this in your view
 	use lav45\aceEditor\AceEditorWidget;
-```
 
-```php
 	echo $form->field($model, 'content')->widget(AceEditorWidget::className(), [
-	    /**
-	     * This configuration of the container in which you will install the editor
-	     * return <div id="editor" style="width: 100%; min-height: 400px;"><div>
-	     */
-		'containerOptions' => [
-		    'id' => 'editor',
-            'style' => 'width: 100%; min-height: 400px;',
-        ],
-
-        /**
-         * JS Settings for AceEditor
-         * details can be found on http://ace.c9.io/#nav=howto
-         */
-        'editorSettings' => "
-            editor.setTheme('ace/theme/idle_fingers');
-            editor.getSession().setMode('ace/mode/html');
-        ",
-	]);
+        'theme' => 'xcode',
+        'mode' => 'html',
+        'showPrintMargin' => false,
+        'fontSize' => 14,
+        'height' => 300,
+        'options' => [
+            'style' => 'border: 1px solid #ccc; border-radius: 4px;'
+        ]
+ 	]);
 
 	// Ace editor without model
 	echo AceEditorWidget::widget([
